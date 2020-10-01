1ce77d5d1161fda727846c597a99847bed2fbdb5
ianmauch <ianmauch@100.64.15.183>
2020-10-01 12:40:05 -0700

    Update the AddingMachine class to include correct functionality for the getTotal(), add(int value), subtract(int value), toString(), and clear() functions.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,32 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String history = "0";
 
 	public AddingMachine() {
 		total = 0; // not needed - included for clarity
 	}
 
 	public int getTotal() {
-		return 0;
+		return total; // returns the total
 	}
 
 	public void add(int value) {
+		total += value; // adds the parameter value to total
+		history += " + " + value; // adds " + " and the value to the string history
 	}
 
 	public void subtract(int value) {
+		total -= value; // subtracts the parameter value from total
+		history += " - " + value; // adds " - " and the value to the string history
 	}
 
 	public String toString() {
-		return "";
+		return history; // returns the string history
 	}
 
 	public void clear() {
+		total = 0; // sets total to 0
+		history = "0"; // sets history to "0"
 	}
 }
---
+++ cse360assignment02/src/cse360assignment02/AddingMachineDriver.java
@@ -0,0 +1,19 @@
+package cse360assignment02;
+
+public class AddingMachineDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		System.out.println(myCalculator.toString());
+		System.out.println(myCalculator.getTotal());
+		myCalculator.clear();
+		myCalculator.add(99);
+		System.out.println(myCalculator.toString());
+		System.out.println(myCalculator.getTotal());
+	}
+
+}
